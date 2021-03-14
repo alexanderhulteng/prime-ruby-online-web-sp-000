c814115fe17bbbaedc4d45283b8d60ba3ec3ad1d
Alexander Hulteng <alexanderhulteng@gmail.com>
2021-03-14 05:47:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+
+  puts "Enter the number:"
+  num=gets.chomp.to_i
+  count=0
+  if (num<=1)
+  	puts "false"
+  else
+
+  	i=2
+  	while(i<num)
+  		if (num%i==0)
+  			puts "false"
+      elsif (num%i==1)
+        puts "true"
+  		end
+  		i+=1
+  	end
+
+  end
